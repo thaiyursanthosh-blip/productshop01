383de8a4b8d353ccde76b05aefb8aee59a169c06
Bavani Santhosh <sbavani@jiditsolutions.com>
2026-08-05 22:19:38 +0000

    Testing Rest and Odata

--- srv/service.cds
+++ srv/service.cds
@@ -1,3 +1,13 @@
+
+@pprotocol: 'rest'
+
 service product {
     function printhelloworld (input: String) returns String;
+
+    function addition (num1: Integer, num2: Integer) returns Integer;
+
+    @open
+
+    type object {};
+    function MyFunction(category: Integer) returns object;
 }
--- srv/service.js
+++ srv/service.js
@@ -5,5 +5,31 @@ module.exports = cds.service.impl(async function(srv){
     srv.on('printhelloworld', req => {
         console.log (req.data) //Print the request coming from server
         return `${req.data.input} World`
-        })
+    })
+
+    srv.on('addition', req =>{
+        console.log (req.data)
+        let result = req.data.num1 + req.data.num2;
+        return result
+    }) 
+    
+    srv.on('MyFunction', req =>{
+        let result = {}
+
+        if(req.data.category == 1){
+            result.product = 'BMW',
+            result.price = '1200 USD',
+            result.location = 'New Delhi'
+        }else{
+            result.product = 'Audi',
+            result.stock =120,
+            result.priceArray = [
+                {
+                    "Price" : 47457,
+                    "Discount":"20 %"
+                }
+            ]
+        }
+        return result;
+    })
 })
